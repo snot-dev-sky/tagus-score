4046d7deff20db0cb198bab04ae2c7c8a7d05788
snot-dev-sky <snot.dev.sky@gmail.com>
2026-04-17 23:51:58 +0100

    fixed express imports

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
+import type { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
