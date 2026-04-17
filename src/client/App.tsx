7b1c9f9d5a1720511db916ac58e3029a6e91e480
snot-dev-sky <snot.dev.sky@gmail.com>
2026-04-18 00:49:25 +0100

    added lead file and colors css

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LeadForm from './Lead-Form';
 import './styles/global.css';
 
 const App: React.FC = () => {
   return (
-    <div className="app-container">
-      <h1>Welcome to Tagus Score</h1>
-      <p>React + Express + TypeScript Boilerplate</p>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <h1>Welcome to Tagus Score</h1>
+              <p>React + Express + TypeScript Boilerplate</p>
+            </>
+          } />
+          <Route path="/lead-form" element={<LeadForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/client/Lead-Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LeadForm: React.FC = () => {
+  return (
+    <div>
+      <h1>Lead Form</h1>
+      <p>This is a simple lead form component.</p>
+    </div>
+  );
+};
+
+export default LeadForm;
